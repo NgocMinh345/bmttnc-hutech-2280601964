fb319e2dad4a6c9ef263d9612e7ef2286b44a96d
NgocMinh345 <tranminh2022004@gmail.com>
2025-03-27 17:44:59 +0700

    Delete lab01 directory

--- lab01/ex02.py
+++
@@ -1,6 +0,0 @@
-# Nhập bán kính từ người dùng
-bankinh = input("Nhập bán kính của hình tròn: ")
-#Tính diện tích của hình tròn
-dientich = 3.14 * (bankinh ** 2)
-# In diện tích của hình tròn ra màn hình
-print("Diện tích của hình tròn là:", dientich)
--- lab01/ex05.py
+++
@@ -1,8 +0,0 @@
-sogiolam = float(input("Số giờ làm mỗi tuần: "))
-luonggio = float(input("Lương mỗi giờ: "))
-giotieuchuan = 44 #số giờ làm chuẩn mỗi tuần
-#số giờ làm vượt chuẩn mỗi tuần
-sogiovuotchuan = max(0, sogiolam - giotieuchuan)
-#tính tổng thu nhập
-thuclinh =giotieuchuan * luonggio + sogiovuotchuan * luonggio * 1.5
-print("Tổng thu nhập hàng tháng của bạn là: ", thuclinh)
